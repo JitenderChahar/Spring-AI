0a4c474d8cb455d3404ca05917480bb81d5fc1f5
Jitender Chahar <jittuchahar@hotmail.com>
2026-01-15 20:52:25 -0500

    Adding Qdrant vector store support

--- src/main/java/com/dev/jc/controller/AskController.java
+++ src/main/java/com/dev/jc/controller/AskController.java
@@ -14,18 +14,18 @@ import reactor.core.publisher.Flux;
 
 @RestController
 public class AskController {
-	
+
 	private final SpringAiBoardGameService askService;
-	
+
 	public AskController(SpringAiBoardGameService askService) {
 		this.askService = askService;
 	}
-	
+
 	@PostMapping(value = "/ask", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Answer askQuestion(@RequestBody @Valid Question question) {
 		return askService.askQuestion(question);
 	}
-	
+
 	@PostMapping(value = "/ask-streaming", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public Flux<String> askQuestionStreaming(@RequestBody @Valid Question question) {
 		return askService.askQuestionStreaming(question);
